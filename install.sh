cb6f5e9604aa1b3c1e1a41ba173eec45f19e0f27
quniu <tandounet@outlook.com>
2022-09-08 16:48:08 +0800

    bug fix

--- install.sh
+++ install.sh
@@ -227,7 +227,14 @@ install_XrayR() {
     echo "------------------------------------------"
 }
 
+modify_time(){
+    timedatectl set-timezone Asia/Shanghai
+    echo "LC_TIME=en_DK.UTF-8" >> /etc/default/locale && cat /etc/default/locale
+    ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+}
+
 echo -e "${green}开始安装${plain}"
+modify_time
 install_base
 check_nginx
 install_acme
